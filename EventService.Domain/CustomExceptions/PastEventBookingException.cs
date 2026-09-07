1b1c47618e7fdba8d2745eec6cefcbf52e37f6f6
Sergey Chikanov <79099045399@yandex.ru>
2026-09-07 14:38:45 +0300

    Delete legacy.

--- EventService.Domain/CustomExceptions/PastEventBookingException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EventService.Domain.CustomExceptions
-{
-    public class PastEventBookingException : Exception
-    {
-        public PastEventBookingException() : base("You cannot book an event that has already taken place.") { }
-
-        public PastEventBookingException(string message) : base(message) { }
-    }
-}
