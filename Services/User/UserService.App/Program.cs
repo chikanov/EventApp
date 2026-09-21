fc5420194ba1cb747054566f9551bc6ad2835dc8
Sergey Chikanov <79099045399@yandex.ru>
2026-09-21 21:39:38 +0300

    Add observability to shared. Add provisioning in grafana/

--- Services/User/UserService.App/Program.cs
+++ Services/User/UserService.App/Program.cs
@@ -27,50 +27,7 @@ builder.Services.AddControllersWithViews()
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
 
-var serviceName = builder.Configuration.GetValue<string>("ServiceName")
-                ?? throw new InvalidOperationException("ServiceName not found.");
-builder.Services
-    .AddOpenTelemetry()
-    .ConfigureResource(resource => resource
-        .AddService(
-            serviceName: serviceName))
-    .WithMetrics(metrics =>
-    {
-        metrics
-            .AddAspNetCoreInstrumentation()
-            .AddRuntimeInstrumentation()
-            .AddPrometheusExporter();
-    })
-    .WithTracing(tracing => tracing
-        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
-        .AddAspNetCoreInstrumentation(options =>
-        {
-            options.Filter = httpContext =>
-            {
-                var path = httpContext.Request.Path;
-
-                return !path.StartsWithSegments("/health") &&
-                       !path.StartsWithSegments("/metrics");
-            };
-        })
-        .AddHttpClientInstrumentation()
-        .AddEntityFrameworkCoreInstrumentation()
-        .AddOtlpExporter(options =>
-        {
-            var otlpExporterEndpoint = builder.Configuration.GetValue<string>("Otlp:Endpoint")
-                ?? throw new InvalidOperationException("Otlp:Endpoint not found.");
-            options.Endpoint = new Uri(otlpExporterEndpoint);
-
-            options.Protocol = OtlpExportProtocol.HttpProtobuf;
-
-            options.BatchExportProcessorOptions.ScheduledDelayMilliseconds = 1000;
-
-            options.BatchExportProcessorOptions.ExporterTimeoutMilliseconds = 5000;
-        }));
-
-builder.Host.UseSerilog((ctx, cfg) =>
-    cfg.ReadFrom.Configuration(ctx.Configuration)
-       .WriteTo.Console(new CompactJsonFormatter()));
+ObservabilityServiceCollectionExtensions.AddObservabilityServices(builder);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
                 ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -141,7 +98,7 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapPrometheusScrapingEndpoint();
+ObservabilityServiceCollectionExtensions.MapPrometheusScrapingEndpoint(app);
 app.MapControllers();
 
 app.Run();
